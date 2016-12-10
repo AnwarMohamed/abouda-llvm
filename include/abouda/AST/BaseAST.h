59441bde840741fda4df15c2f46cd59b8f51ce42
AnwarMohamed <anwarelmakrahy@gmail.com>
2016-12-10 18:41:12 +0200

    changing skeleton

---
+++ include/abouda/AST/BaseAST.h
@@ -0,0 +1,10 @@
+#ifndef BASE_AST_H
+#define BASE_AST_H
+
+class BaseAST {
+public:
+	BaseAST();
+	~BaseAST();
+};
+
+#endif
---
+++ include/abouda/Parser/BaseParser.h
@@ -0,0 +1,10 @@
+#ifndef BASE_PARSER_H
+#define BASE_PARSER_H
+
+class BaseParser {
+public:
+	BaseParser();
+	~BaseParser();
+};
+
+#endif
